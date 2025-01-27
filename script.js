3bbe0ef77b8041cd042a6afed3c05f8a3d8b543e
mvgui <michellyvg@yahoo.com.br>
2025-01-26 21:03:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//Entrada
+let tempo = Number(prompt("Digite o tempo em segundos:"));
+let distancia = Number(prompt("Digite a distância em metros:"));
+
+//Processamento
+let velocidadeMedia = distancia / tempo;
+
+//Saída
+alert("A velocidade média é de " + velocidadeMedia + "m/s.");
